e2029b77a9e466b6a971cea197a5bc81b3a1c288
NJaua <nicojaua@gmail.com>
2019-05-17 10:48:30 -0400

    Creating each goverment branch and nothing else

---
+++ legis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <linux/limits.h>
+
+#include "legis.h"
+
+int main(int argc, char **argv){
+	
+}
