d73b0403d07449a1f424eb025111ecc54fe1a19a
MarcosJLR <socram.693@gmail.com>
2019-05-27 19:20:11 -0400

    stats threads fixed

--- threads/prensaThread.h
+++ threads/prensaThread.h
@@ -23,7 +23,7 @@ typedef struct contactInfo{
 typedef struct threadArguments{
 	char headline[10][MAX_ACT_LINE];
 	sem_t write, press, full;
-	int head, end, days, stats[3];
+	int head, end, day;s
 	char dir[PATH_MAX];	
 	ContactInfo *c;
 	int stats[3];
